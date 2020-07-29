aa0387c5b0f5eae682247387feea60b6648bef7a
Saul Ytucayasi <saul.ytucayasi@upeu.edu.pe>
2020-07-28 22:01:28 -0500

    hello world

--- __py__/__init__.py
+++ __py__/__init__.py
@@ -17,5 +17,78 @@ class Students(Person):
 x = Students("Saul", "Ytucayasi", 2003)
 x.welcome()
 
+for x in [1, 2, 3, 4, 5]:
+  print(f"La variable es {x} su cuadrado es {x**2}")
+
+x = 20
+print("Tabla de multiplicar advance")
+for x in [1, 2, 3, 4, 5]:
+  print(f"{x} x {x+1} = {x*(x+1)}")
+
+for x in "FORM":
+  print(f" La letra es: {x}")
+
+times = int(input("Ingrese la cantidad de veces: "))
+for x in range(times):
+  print("Hello")
+
+count = 0
+for x in range(1, 6):
+  if x % 2 == 0:
+    count += 1
+print(f"Se hallaron {count} multiplos de 2")
+
+find = False
+for x in range(1, 6):
+  if x % 2 == 0:
+    find = True
+if find:
+  print(f"Verdadero")
+else:
+  print(f"Falso")
+
+x = 1
+while x <= 5:
+  print(x)
+  x += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
